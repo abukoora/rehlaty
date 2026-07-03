ec5307c657ae061780d288eef16e9305c278a9ce
abukoora <albytalsydasrty@gmail.com>
2026-07-04 00:03:59 +0300

    Update config.jshqhtm اضافة رقم الجوال

--- js/config.js
+++ js/config.js
@@ -8,7 +8,7 @@ export const CONFIG = {
 
   // رقم الواتساب الافتراضي للدعم (بصيغة دولية بدون + أو صفر البداية)
   // مثال مصر: 201xxxxxxxxx | مثال السعودية: 9665xxxxxxxx
-  WHATSAPP_DEFAULT_NUMBER: "201000000000",
+  WHATSAPP_DEFAULT_NUMBER: "201064526023",
 
   APP_NAME: "رحلة الحياة الزوجية",
 };
